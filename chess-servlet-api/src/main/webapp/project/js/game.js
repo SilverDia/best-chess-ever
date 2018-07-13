fe9e700d13ffd036efd71baeda769d8c08ef99eb
Lea Linux Mint <lea.werner.91@gmail.com>
2018-07-13 07:29:14 +0200

    timer raedy, html and css updated (responsive layout)

--- chess-servlet-api/src/main/webapp/project/js/game.js
+++ chess-servlet-api/src/main/webapp/project/js/game.js
@@ -2,161 +2,170 @@ var jsonObject = {};
 var gameID;
 
 $(document).ready(function () {
-	init_game()
+    init_game(gameID);
+    $('#pause-button').click(function () {
+        stopTimer(true);
+    });
+    $('#pause-layer').click(function () {
+        startTimer(true);
+    });
 });
 
 function init_game() {
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState === 4 && this.status === 200) {
-			jsonObject = JSON.parse(this.responseText);
-			parse_json();
-		}
-	};
-	xhttp.open("GET",
-		"http://localhost:8080/ChessGame/GetChessboardServlet?action=init-game",
-		true);
-	xhttp.send();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            jsonObject = JSON.parse(this.responseText);
+            parse_json();
+            startTimer();
+        }
+    };
+    xhttp.open("GET",
+        "http://localhost:8080/ChessGame/GetChessboardServlet?action=init-game&game-id=" + gameID,
+        true);
+    xhttp.send();
 }
 
 function parse_json() {
-	clearOldSuggestions();
-	gameID = jsonObject.gameId;
-	
-	document.getElementById('game-turn-info-container').innerHTML = jsonObject.activePlayer + " ist am Zug!";
-	for (message in jsonObject.gamelog) {
-		document.getElementById('game-log-textarea').innerHTML += jsonObject.gamelog[message] + '\n';
-	}
-	
-	var i;
-	for (i = 0; i < 8; i++) {
-		for (square in jsonObject.board.chessBoard[i]) {
-			if (jsonObject.board.chessBoard[i][square].pieceID !== "") {
-				document
-						.getElementById(jsonObject.board.chessBoard[i][square].squareId).innerHTML = "<img src=/ChessGame/project/resources/images/piece-sets/default/"
-						+ jsonObject.board.chessBoard[i][square].pieceID
-								.substring(
-										0,
-										jsonObject.board.chessBoard[i][square].pieceID.length - 2)
-								.toLowerCase() + ".png\>";
-			} else {
-				document
-						.getElementById(jsonObject.board.chessBoard[i][square].squareId).innerHTML = "";
-			}
-		}
-	}
-	setupOnclick('WHITE');
-	setupOnclick('BLACK');
+    clearOldSuggestions();
+    gameID = jsonObject.gameId;
+
+    document.getElementById('game-turn-info-container').innerHTML = jsonObject.activePlayer + " ist am Zug!";
+    for (message in jsonObject.gamelog) {
+        document.getElementById('game-log-textarea').innerHTML += jsonObject.gamelog[message] + '\n';
+    }
+
+    var i;
+    for (i = 0; i < 8; i++) {
+        for (square in jsonObject.board.chessBoard[i]) {
+            if (jsonObject.board.chessBoard[i][square].pieceID !== "") {
+                document
+                    .getElementById(jsonObject.board.chessBoard[i][square].squareId).innerHTML = "<img src=/ChessGame/project/resources/images/piece-sets/default/"
+                    + jsonObject.board.chessBoard[i][square].pieceID
+                        .substring(
+                            0,
+                            jsonObject.board.chessBoard[i][square].pieceID.length - 2)
+                        .toLowerCase() + ".png\>";
+            } else {
+                document
+                    .getElementById(jsonObject.board.chessBoard[i][square].squareId).innerHTML = "";
+            }
+        }
+    }
+    setupOnclick('WHITE');
+    setupOnclick('BLACK');
 }
 
 function setupOnclick(color) {
-	for (pieceType in jsonObject.player[color].pieceSet) {
-		for (piece in jsonObject.player[color].pieceSet[pieceType]) {
-			if (typeof jsonObject.player[color].pieceSet[pieceType][piece].possibleMoves !== 'undefined')
-				document
-						.getElementById(jsonObject.player[color].pieceSet[pieceType][piece].positionSquareId).firstChild
-						.setAttribute("onclick", "clickedPiece('" + color
-								+ "','" + pieceType + "','" + piece + "')");
-		}
-	}
-}
-
-function clickedPiece(color, pieceType, piece) {
-	clearOldSuggestions();
-
-	var selected = document
-			.getElementById(jsonObject.player[color].pieceSet[pieceType][piece].positionSquareId);
-	selected.classList.add("click-display");
-	selected.classList.add("chosen");
-
-	for (move in jsonObject.player[color].pieceSet[pieceType][piece].possibleMoves) {
-		var element = document
-				.getElementById(jsonObject.player[color].pieceSet[pieceType][piece].possibleMoves[move].moveToSquareId);
-		element
-				.setAttribute(
-						"onclick",
-						"madeMove('"
-								+ piece
-								+ "','"
-								+ jsonObject.player[color].pieceSet[pieceType][piece].possibleMoves[move].moveToSquareId
-								+ "')");
-
-		if (element.innerHTML !== "") {
-			element.classList.add("capture");
-		} else {
-			element.classList.add("valid-turn");
-		}
-		element.classList.add("click-display");
-	}
-}
-
-function clearOldSuggestions() {
-	$('.click-display').toggleClass("click-display capture valid-turn chosen",
-			false).removeAttr("onclick");
-}
-
-function madeMove(pieceId, moveToSquareId) {
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			jsonObject = JSON.parse(this.responseText);
-			parse_json();
-		}
-	};
-	xhttp
-			.open(
-					"GET",
-					"http://localhost:8080/ChessGame/GetChessboardServlet?action=execute-move&game-id="
-							+ gameID
-							+ "&move-piece-id="
-							+ pieceId
-							+ "&move-to-square-id=" + moveToSquareId, true);
-	xhttp.send();
-}
-
-
-var s = 0;
-var m = 0;
-var t;
-var timer_is_on = 0;
-
-function runTimer() {
-    s = s + 1;
-    formatTimer(s, m);
-    t = setTimeout(runTimer, 1000);
-}
-
-function startTimer(pause) {
-    if (!timer_is_on) {
-        timer_is_on = 1;
-        runTimer();
-        if (pause) {
-            $('#pause-layer').slideToggle("500");
+    for (pieceType in jsonObject.player[color].pieceSet) {
+        for (piece in jsonObject.player[color].pieceSet[pieceType]) {
+            if (typeof jsonObject.player[color].pieceSet[pieceType][piece].possibleMoves !== 'undefined')
+                document
+                    .getElementById(jsonObject.player[color].pieceSet[pieceType][piece].positionSquareId).firstChild
+                    .setAttribute("onclick", "clickedPiece('" + color
+                        + "','" + pieceType + "','" + piece + "')");
         }
     }
 }
 
-function stopTimer(pause) {
-    clearTimeout(t);
-    timer_is_on = 0;
+function clickedPiece(color, pieceType, piece) {
+    clearOldSuggestions();
 
-    if (!pause) {
-        $('.active-time img.time-button').toggleClass("time-pause time-continue",false);
-        s = 0;
-        m = 0;
-    } else {
-        $('#pause-layer').slideToggle("500");
+    var selected = document
+        .getElementById(jsonObject.player[color].pieceSet[pieceType][piece].positionSquareId);
+    selected.classList.add("click-display");
+    selected.classList.add("chosen");
+
+    for (move in jsonObject.player[color].pieceSet[pieceType][piece].possibleMoves) {
+        var element = document
+            .getElementById(jsonObject.player[color].pieceSet[pieceType][piece].possibleMoves[move].moveToSquareId);
+        element
+            .setAttribute(
+                "onclick",
+                "madeMove('"
+                + piece
+                + "','"
+                + jsonObject.player[color].pieceSet[pieceType][piece].possibleMoves[move].moveToSquareId
+                + "')");
+
+        if (element.innerHTML !== "") {
+            element.classList.add("capture");
+        } else {
+            element.classList.add("valid-turn");
+        }
+        element.classList.add("click-display");
     }
-    formatTimer(s, m)
 }
 
-function formatTimer() {
-    if (s === 60) {
-        s = 0;
-        m += 1;
-    }
+function clearOldSuggestions() {
+    $('.click-display').toggleClass("click-display capture valid-turn chosen",
+        false).removeAttr("onclick");
+}
 
-    $('.grid-time.active-time span').text(checkTime(m) + ":" + checkTime(s));
+function madeMove(pieceId, moveToSquareId) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            jsonObject = JSON.parse(this.responseText);
+            parse_json();
+            startTimer();
+        }
+    };
+    xhttp
+        .open(
+            "GET",
+            "http://localhost:8080/ChessGame/GetChessboardServlet?action=execute-move&game-id="
+            + gameID
+            + "&move-piece-id="
+            + pieceId
+            + "&move-to-square-id=" + moveToSquareId
+            + "&duration=" + secTotal, true);
+    xhttp.send();
+    stopTimer();
+}
+
+var secTotal = 0;
+var t;
+var timerStatus = 0;
+
+function runTimer() {
+    secTotal++;
+    formatTime();
+    t = setTimeout(runTimer, 1000);
+}
+
+function startTimer(paused) {
+    if (!paused) {
+        stopTimer();
+    } else {
+        $('.grid-game').slideToggle(500);
+        $('#pause-layer').slideToggle(500);
+    }
+    timerStatus = 1;
+    runTimer();
+}
+
+function stopTimer(pause) {
+    clearTimeout(t);
+    timerStatus = 0;
+
+    if (!pause) {
+        secTotal = 0;
+    } else {
+        $('.grid-game').slideToggle(500);
+        $('#pause-layer').slideToggle(500);
+    }
+    formatTime();
+}
+
+function formatTime() {
+    var m;
+    var s;
+    m = Math.floor(secTotal / 60);
+    s = secTotal % 60;
+
+    $('#game-timer-' + (jsonObject.activePlayer).toLowerCase())
+        .text(checkTime(m) + ":" + checkTime(s));
 }
 
 function checkTime(i) {
@@ -165,22 +174,4 @@ function checkTime(i) {
     }
     // add zero in front of numbers < 10
     return i;
-}
-
-function triggerConversion(player) {
-    $('#pawn-conversion-' + player).slideToggle(500);
-}
-
-function changePlayer() {
-
-    stopTimer();
-    $('.grid-time').toggleClass('active-time inactive-time');
-    $('.active-time img.time-button').toggleClass("time-pause",true);
-
-    $('.player-content-block').toggleClass('active-player inactive-player');
-    $('.turn-info-content')
-        .text(
-            "Spieler " + $('.grid-player.active-player .grid-name').text()
-            + " ist am Zug");
-    startTimer();
 }
